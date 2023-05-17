0ea6fdacc79c996af097361730102933e26dfc5f
serhii <serhii@svipe.com>
2023-05-17 11:24:47 +0200

    Initial Commit

---
+++ SpotifyPie/SpotifyPieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyPieApp.swift
+//  SpotifyPie
+//
+//  Created by Serhii Trusov on 2023-05-17.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyPieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
